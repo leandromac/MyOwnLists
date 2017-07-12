0555c8710f84e5957d33019c38be09261bb9a917
Leandro Macedo <nandofmacedo@gmail.com>
2017-07-12 13:59:14 +0000

    Order by desc list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -4,7 +4,7 @@ class TodoListsController < ApplicationController
   # GET /todo_lists
   # GET /todo_lists.json
   def index
-    @todo_lists = TodoList.all
+    @todo_lists = TodoList.order("created_at DESC")
   end
 
   # GET /todo_lists/1
